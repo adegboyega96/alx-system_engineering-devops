ddd0a65904c5234d96e807cc64d3fbb56b35e1a4
Emmanuel Adetoro <bizemmang@gmail.com>
2023-05-03 09:18:22 -0700

    update task

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# Repetition Token #0
+# Repetition Token #1
 puts ARGV[0].scan(/hb?tn/).join
--- 0x06-regular_expressions/3-repetition_token_2.rb
+++ 0x06-regular_expressions/3-repetition_token_2.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# Repetition Token #0
-puts ARGV[0].scan(/hbt{1,4}n/).join
+# Repetition Token #2
+puts ARGV[0].scan(/hbt+n/).join
