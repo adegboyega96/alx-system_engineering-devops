851443a42435a4d92c28a21f77dfbfa5213b1e97
Emmanuel Adetoro <bizemmang@gmail.com>
2023-05-03 09:07:02 -0700

    update task

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# regular expression that matches the word school
+puts ARGV[0].scan(/School/).join
---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+# Repetition Token #0
+puts ARGV[0].scan(/hb?tn/).join
