1e2c6447c88e3a7aad4a86f924c5bc01a8492dfa
Emmanuel Adetoro <bizemmang@gmail.com>
2023-06-29 07:27:12 -0700

    create file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,5 +1,7 @@
+# create file with certain requirements
 file { '/tmp/school':
   ensure  => file,
+  path    => '/tmp/school'
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
